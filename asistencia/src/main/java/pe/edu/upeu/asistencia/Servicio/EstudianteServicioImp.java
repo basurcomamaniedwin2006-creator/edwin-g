760a2ce7212e09d875796c194d70a3109dae349c
ruben7405 <rubenxtlv075@gmail.com>
2025-08-28 12:20:23 -0500

    trabajoPOO

---
+++ asistencia/src/main/java/pe/edu/upeu/asistencia/Servicio/EstudianteServicioImp.java
@@ -0,0 +1,38 @@
+
+
+package pe.edu.upeu.asistencia.Servicio;
+
+import pe.edu.upeu.asistencia.modelo.Estudiante;
+import pe.edu.upeu.asistencia.repositorio.EstudianteRepository;
+
+import java.util.List;
+
+public class EstudianteServicioImp extends EstudianteRepository implements EstudianteServicioI {
+
+    @Override
+    public void save(Estudiante estudiante) {
+        estudiantes.add(estudiante);
+    }
+    @Override
+    public List<Estudiante> findAllestudiante() {
+        if(estudiantes.size()==1){
+            return super.findAllEstudiantes();
+        }
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante ubdateEstudiante(Estudiante estudiante, int index) {
+        return  estudiantes.set(index,estudiante);
+    }
+
+    @Override
+    public void delete(int index) {
+
+    }
+
+    @Override
+    public Estudiante findEstudianteById(int index) {
+        return null;
+    }
+}
